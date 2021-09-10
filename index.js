7b2916c2dcdbb6b3c45d254f15f172d082681d61
Zorba11 <zorbathegreek111@gmail.com>
2021-09-10 01:10:58 -0600

    final

--- index.js
+++ index.js
@@ -17,4 +17,12 @@ const io = socket(server);
 
 io.on('connection', (socket) => {
   console.log('made the socket connection!', socket.id);
+
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data);
+  });
+
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data);
+  });
 });
